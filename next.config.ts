a98f6c530407d7d99f735c296f88f0dc88e7de74
Aakash Dixit <aakash.dixit@natwest.com>
2025-05-10 21:42:27 +0530

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,6 @@
+// This file allow to configure Next.js.
+// This file is to add additional customizations to Next.js.
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,3 +1,4 @@
+// This is the postcss configuration. A tool to process CSS with different plugins. In this case, it is used to add Tailwind CSS to the project.
 const config = {
   plugins: ["@tailwindcss/postcss"],
 };
