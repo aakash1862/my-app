aa49c852de95df37678e6a24f80f0eb82c5c4890
Aakash Dixit <aakash.dixit@natwest.com>
2025-06-04 23:08:41 +0530

    Commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div>
-            <h1 className="text-3xl">NAVBAR OF ABOUT</h1>
-            {children}
+        <div className="bg-black-100 min-h-screen">
+            <nav className="bg-blue-600 text-white p-4 shadow-md">
+                <h1 className="text-3xl font-bold">NAVBAR</h1>
+            </nav>
+            <main className="container mx-auto p-4">
+                {children}
+            </main>
         </div>
     );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,38 @@
 import Image from "next/image";
 import Hello from "../components/hello";
 
-export default function Home() {
+// A way to call Metadata for each page, this will take precedence over the metadata in layout.tsx
+// export async function generateMetadata() {
+//   return {
+//     title: "Home",
+//     description: "This is the home page of my full stack app",
+//   };
+// }
+
+async function Home() {
   console.log("Hello from the Home component -- Server component"); /** This will be visible on Console of Developer Tools on browser. */
+  
+  // Fetch albums data
+  const response = await fetch("https://jsonplaceholder.typicode.com/albums");
+  if (!response.ok) throw new Error("Failed to fetch data");
+  const albums = await response.json();
+  
   return (
     <>
-      <h1 className="text-3xl">Welcome to my-app full stack app</h1>
+      <h1 className="text-3xl bg-black-500 text-white p-4 shadow-md">Welcome to my-app full stack app</h1>
       <Hello />
+      
+      {/* Albums grid */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-6">
+        {albums.map((album: {id: number, title: string }) => (
+          <div key={album.id} className="bg-white shadow-md rounded-lg p-4 transition hover:shadow-lg">
+            <h3 className="text-xl font-bold mb-2">Album Title: {album.title}</h3>
+            <p className="text-gray-600">Album ID: {album.id}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
 
-/** Any change will be visible on the UI immediately because of HMR(Hot Module Replacement) in Next.js. */
+export default Home;
